c47d9a59ef7eee534551939e1c8408fe8c4f0ccb
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2025-08-31 12:29:17 +0300

    Resolve ticket issue #4: Fix Pydantic Schema for Item

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -2,10 +2,13 @@ from pydantic import BaseModel
 
 class ItemBase(BaseModel):
     name: str
-    description: str = None
+    description: str | None = None
 
 class ItemCreate(ItemBase):
     pass
 
 class ItemSchema(ItemBase):
     id: int
+    
+    class Config:
+        orm_mode = True
